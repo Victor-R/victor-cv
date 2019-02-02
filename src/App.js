20a6c905b8ffeb247d7d582ab5f8b3bb79972d12
Victor Ribeiro <victor_ribeiro.pira@hotmail.com>
2019-02-01 23:48:03 -0200

    Initial Layout, css back animation and started Resume Component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,86 @@
 import React, { Component } from 'react';
 import { Container, Col, Row } from 'reactstrap';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
+import Resume from './Resume';
 import './App.css';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
-import { dark } from '@material-ui/core/styles/createPalette';
 
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			teste: false
+			showResume: false,
 		}
 	}
 
+	// Customiza o componente Paper do materialUI
 	paperStyle = () => createMuiTheme({
+		typography: {
+			useNextVariants: true,                  // tipografia padrão do materialUI            
+		},
 		palette: {
 			type: 'dark'
 		}
 	});
 
+	// Customiza estilos dos botões de MaterialUI
+	btnCustomStyles = () => createMuiTheme({
+		typography: {
+			useNextVariants: true,                  // tipografia padrão do materialUI            
+		},
+		palette: {
+			primary: {
+				main: '#22569f'                     // Altera paleta de cores dos botões
+			}
+		}
+	});
+
+	// Customiza o Componente Typography do MaterialUI
+	typoCustomStyle = () => createMuiTheme({
+		typography: {
+			useNextVariants: true,                  // tipografia padrão do materialUI            
+		},
+		palette: {
+			primary: {
+				main: '#fff'                     // Altera paleta de cores dos botões
+			}
+		}
+	});
+
+	handleBtnShowResume = () => {
+		this.setState({
+			showResume: !this.state.showResume
+		});
+	}
+
 	render() {
 		return (
-			<Container fluid>
+			<Container className='d-flex justify-content-center'>
 				<MuiThemeProvider theme={this.paperStyle()}>
-					<Paper className='mainBack col-10' dark >
-						<Row>
-							<Col md='2'>
-								<div style={{ textAlign: 'center' }}>
-									<Avatar alt="Remy Sharp" src="perfil.png" style={{ width: '250px', height: '250px', textAnchor: 'middle' }} />
-									<Typography variant='overline' style={{ textAnchor: 'middle' }}>Victor Henrique Ribeiro</Typography>
-								</div>
+					<Paper className='mainBack' >
+						<Row className="d-flex align-items-center">
+							<Col md='3' style={{ maxWidth: '100%' }}>
+								<Avatar src="perfil.png" style={{ width: '250px', height: '250px' }} />
+							</Col>
+							<Col>
+								<Row>
+									<MuiThemeProvider theme={this.typoCustomStyle()}>
+										<Typography variant='h3' color='primary' gutterBottom>Victor Henrique Ribeiro</Typography>
+									</MuiThemeProvider>
+								</Row>
+								<Row className='d-flex justify-content-center'>
+									<MuiThemeProvider theme={this.btnCustomStyles()}>
+										<Button variant='contained' color='primary' onClick={this.handleBtnShowResume}>show Resume</Button>
+									</MuiThemeProvider>
+								</Row>
+								{
+									this.state.showResume ? <Resume /> : ''
+								}
+								
 							</Col>
 						</Row>
 					</Paper>
---
+++ src/Resume.js
@@ -0,0 +1,23 @@
+/*Author: Victor Henrique Ribeiro - Date: 01/02/2019 
+Component: Resume - Language: Javascript - 
+Desc: A component where's all the content of Resume, that is skills, projects, jobs, occupations, etc..*/
+import React, { Component } from 'react';
+
+class Resume extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                WWWOOOOWW
+            </div>
+        )
+    }
+}
+
+export default Resume;
