a05236b0025b13e5e429e9efb2b73d22cd5e5d2f
Victor Ribeiro <victor_ribeiro.pira@hotmail.com>
2019-02-06 18:35:46 -0200

    Updates

---
+++ precache-manifest.436ac8a29992dc28718641634b11e59a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77b0e037137fe17d86a3",
+    "url": "/victor-resume/static/js/runtime~main.77b0e037.js"
+  },
+  {
+    "revision": "58f6b6162c3de9cccbe8",
+    "url": "/victor-resume/static/js/main.58f6b616.chunk.js"
+  },
+  {
+    "revision": "c675e9ca9962b5270c79",
+    "url": "/victor-resume/static/js/1.c675e9ca.chunk.js"
+  },
+  {
+    "revision": "58f6b6162c3de9cccbe8",
+    "url": "/victor-resume/static/css/main.029a9b68.chunk.css"
+  },
+  {
+    "revision": "9f8a65ee2f583635c285fa4cea23f6be",
+    "url": "/victor-resume/index.html"
+  }
+];
