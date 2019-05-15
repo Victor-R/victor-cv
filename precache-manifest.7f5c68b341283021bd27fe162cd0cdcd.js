da130d2cc1cec64f89d6773b58f0dbf00cf754b2
Victor Ribeiro <victor_ribeiro.pira@hotmail.com>
2019-05-14 21:15:07 -0300

    Updates

---
+++ precache-manifest.7f5c68b341283021bd27fe162cd0cdcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4957e11710b361958fb2020b8256278a",
+    "url": "/victor-cv/index.html"
+  },
+  {
+    "revision": "6326ae93b82e8bff005a",
+    "url": "/victor-cv/static/css/main.d7aff9bd.chunk.css"
+  },
+  {
+    "revision": "39d8cfcd1b565cc427f5",
+    "url": "/victor-cv/static/js/2.b2f3cbbe.chunk.js"
+  },
+  {
+    "revision": "6326ae93b82e8bff005a",
+    "url": "/victor-cv/static/js/main.fa33f423.chunk.js"
+  },
+  {
+    "revision": "28a7a8a95831a2a8cca9",
+    "url": "/victor-cv/static/js/runtime~main.37fefae3.js"
+  }
+]);
