13e222dc9f45e9950927737ba6cc51c220eb665f
Victor Ribeiro <victor_ribeiro.pira@hotmail.com>
2019-06-15 01:02:46 -0300

    Finalizado funções e textos

--- src/App.js
+++ src/App.js
@@ -8,120 +8,134 @@ import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown'
 import Resume from './Components/Resume';
 import './css/App.css';
 import { createMuiTheme, MuiThemeProvider, Fab, Switch } from '@material-ui/core';
-import { FaJediOrder, FaEmpire } from 'react-icons/fa';
+import { FaJediOrder, FaEmpire, FaLanguage } from 'react-icons/fa';
 
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			showResume: false,
-			darkMode: true
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            showResume: false,
+            darkMode: true,
+            language: 'pt-br'
+        }
+    }
 
-	// Customiza o componente Paper do materialUI
-	paperStyle = () => createMuiTheme({
-		typography: {
-			useNextVariants: true,                  // tipografia padrão do materialUI            
-		},
-		palette: {
-			type: this.state.darkMode ? 'dark' : 'light'
-		}
-	});
+    // Customiza o componente Paper do materialUI
+    paperStyle = () => createMuiTheme({
+        typography: {
+            useNextVariants: true,
+        },
+        palette: {
+            type: this.state.darkMode ? 'dark' : 'light'
+        }
+    });
 
-	// Customiza estilos dos botões de MaterialUI
-	btnCustomStyles = () => createMuiTheme({
-		typography: {
-			useNextVariants: true,                  // tipografia padrão do materialUI            
-		},
-		palette: {
-			primary: {
-				main: '#22569f'                     // Altera paleta de cores dos botões
-			}
-		}
-	});
 
-	// Customiza o Componente Typography do MaterialUI
-	typoCustomStyle = () => createMuiTheme({
-		typography: {
-			useNextVariants: true,                  // tipografia padrão do materialUI            
-		},
-		overrides: {
-			MuiTypography: {
-				colorTextPrimary: {
-					color: this.state.darkMode ? '#fff' : '#000'
-				}
-			}
-		}
-	});
+    // Customiza estilos dos botões de MaterialUI
+    btnCustomStyles = () => createMuiTheme({
+        typography: {
+            useNextVariants: true,                  // tipografia padrão do materialUI            
+        },
+        palette: {
+            primary: {
+                main: '#22569f'                     // Altera paleta de cores dos botões
+            }
+        }
+    });
 
-	switchCustomStyle = () => createMuiTheme({
-		typography: {
-			useNextVariants: true,                  // tipografia padrão do materialUI            
-		},
-		overrides: {
-			MuiSwitch: {
-				bar: {
-					backgroundColor: this.state.darkMode ? '#424242!Important' : '#fff!Important'
-				},
-				colorPrimary: {
-					color: this.state.darkMode ? '#424242!Important' : '#2196f3!Important'
-				}
-			},
-		}
-	})
 
-	handleBtnShowResume = () => {
-		this.setState({
-			showResume: !this.state.showResume
-		});
-	}
+    // Customiza o Componente Typography do MaterialUI
+    typoCustomStyle = () => createMuiTheme({
+        typography: {
+            useNextVariants: true,                  // tipografia padrão do materialUI            
+        },
+        overrides: {
+            MuiTypography: {
+                colorTextPrimary: {
+                    color: this.state.darkMode ? '#fff' : '#000'
+                }
+            }
+        }
+    });
 
-	render() {
-		return (
-			<Container fluid>
-				<div className='d-inline-flex align-items-center'>
-					<MuiThemeProvider theme={this.switchCustomStyle()}>
-						{this.state.darkMode ? <FaEmpire size={35} color='#383535' /> : <FaJediOrder color='#fff' size={35} />}
-						<Switch color='primary' checked={this.state.darkMode} onChange={() => this.setState({ darkMode: !this.state.darkMode })} />
-					</MuiThemeProvider>
-				</div>
-				<MuiThemeProvider theme={this.paperStyle()}>
-					<Grow in={this.state.showResume} mountOnEnter unmountOnExit>
-						<Row className='d-flex justify-content-center'>
-							<Col lg='10' className='mobile-paper'>
-								<Resume handleBtnShowResume={this.handleBtnShowResume} darkMode={this.state.darkMode} />
-							</Col>
-						</Row>
-					</Grow>
-					<Grow in={!this.state.showResume} mountOnEnter unmountOnExit>
-						<Row className='d-flex justify-content-center'>
-							<Paper className='mainBack'>
-								<Row className="d-flex align-items-center">
-									<Col md='3' className='d-flex justify-content-center' style={{ maxWidth: '100%' }}>
-										<Avatar src="perfil.png" style={{ width: '250px', height: '250px' }} />
-									</Col>
-									<Col>
-										<Row className='justify-content-center'>
-											<MuiThemeProvider theme={this.typoCustomStyle()}>
-												<Typography variant='h3' color='textPrimary' style={{ textAlign: 'center' }} gutterBottom>Victor Henrique Ribeiro</Typography>
-											</MuiThemeProvider>
-										</Row>
-										<Row className='d-flex justify-content-center'>
-											<MuiThemeProvider theme={this.btnCustomStyles()}>
-												<Fab variant='extended' color='primary' onClick={this.handleBtnShowResume}>show resume<KeyboardArrowDown /></Fab>
-											</MuiThemeProvider>
-										</Row>
-									</Col>
-								</Row>
-							</Paper>
-						</Row>
-					</Grow>
-				</MuiThemeProvider>
-			</Container >
-		);
-	}
+    switchCustomStyle = () => createMuiTheme({
+        typography: {
+            useNextVariants: true,                  // tipografia padrão do materialUI            
+        },
+        overrides: {
+            MuiSwitch: {
+                bar: {
+                    backgroundColor: this.state.darkMode ? '#424242!Important' : '#fff!Important'
+                },
+                colorPrimary: {
+                    color: this.state.darkMode ? '#424242!Important' : '#2196f3!Important'
+                }
+            },
+        }
+    })
+
+    handleBtnShowResume = () => {
+        this.setState({
+            showResume: !this.state.showResume
+        });
+    }
+
+    render() {
+        return (
+            <Container fluid>
+                <div className='switch-container'>
+                    <MuiThemeProvider theme={this.switchCustomStyle()}>
+                        <div className='d-inline-flex align-items-center'>
+                            {this.state.darkMode ? <FaEmpire size={35} color='#383535' /> : <FaJediOrder color='#fff' size={35} />}
+                            <Switch color='primary' checked={this.state.darkMode} onChange={() => this.setState({ darkMode: !this.state.darkMode })} />
+                        </div>
+                        <div className='d-inline-flex align-items-center'>
+                            {this.state.darkMode ? <FaLanguage size={35} color='#383535' /> : <FaLanguage color='#fff' size={35} />}
+                            <Switch
+                                color='primary'
+                                checked={this.state.language === 'pt-br'}
+                                onChange={() => this.setState({ language: this.state.language === 'pt-br' ? 'en-us' : 'pt-br' })}
+                            />
+                            <Typography variant='button' color='textPrimary'>{this.state.language}</Typography>
+                        </div>
+                    </MuiThemeProvider>
+                </div>
+                <MuiThemeProvider theme={this.paperStyle()}>
+                    <Grow in={this.state.showResume} mountOnEnter unmountOnExit>
+                        <Row className='d-flex justify-content-center'>
+                            <Col lg='10' className='mobile-paper'>
+                                <Resume handleBtnShowResume={this.handleBtnShowResume} darkMode={this.state.darkMode} language={this.state.language} />
+                            </Col>
+                        </Row>
+                    </Grow>
+                    <Grow in={!this.state.showResume} mountOnEnter unmountOnExit>
+                        <Row className='d-flex justify-content-center'>
+                            <Paper className='mainBack'>
+                                <Row className="d-flex align-items-center">
+                                    <Col md='3' className='d-flex justify-content-center' style={{ maxWidth: '100%' }}>
+                                        <Avatar src="perfil.png" style={{ width: '250px', height: '250px' }} />
+                                    </Col>
+                                    <Col>
+                                        <Row className='justify-content-center'>
+                                            <MuiThemeProvider theme={this.typoCustomStyle()}>
+                                                <Typography variant='h3' color='textPrimary' style={{ textAlign: 'center' }} gutterBottom>Victor Henrique Ribeiro</Typography>
+                                            </MuiThemeProvider>
+                                        </Row>
+                                        <Row className='d-flex justify-content-center'>
+                                            <MuiThemeProvider theme={this.btnCustomStyles()}>
+                                                <Fab variant='extended' color='primary' onClick={this.handleBtnShowResume}>{this.state.language === 'pt-br' ? 'Mostrar Mais' : 'Show More'}<KeyboardArrowDown /></Fab>
+                                            </MuiThemeProvider>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </Paper>
+                        </Row>
+                    </Grow>
+                </MuiThemeProvider>
+            </Container >
+        );
+    }
 }
 
 export default App;
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -7,7 +7,8 @@ import { Col, Row } from 'reactstrap';
 import '../css/Resume.css';
 import Typography from '@material-ui/core/Typography';
 import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp';
-import { createMuiTheme, MuiThemeProvider, Button, Radio } from '@material-ui/core';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { createMuiTheme, MuiThemeProvider, Button, Radio, List, ListItem, ListItemText, IconButton } from '@material-ui/core';
 
 function GenerateRadioInput(props) {
     let items = [];
@@ -22,12 +23,6 @@ function GenerateRadioInput(props) {
 }
 
 class Resume extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
 
     // Customiza o Componente Typography do MaterialUI
     typoCustomStyle = () => createMuiTheme({
@@ -41,29 +36,50 @@ class Resume extends Component {
         }
     });
 
+    btnCustomStyles = () => createMuiTheme({
+        typography: {
+            useNextVariants: true,                  // tipografia padrão do materialUI            
+        },
+        palette: {
+            primary: {
+                main: '#22569f'//'#5E35b1'                     // Altera paleta de cores dos botões
+            }
+        },
+        overrides: {
+            MuiTypography: {
+                colorTextSecondary: {
+                    color: this.props.darkMode ? '#fffcfccc' : ''
+                },
+                body1: {
+                    color: this.props.darkMode ? '#fff' : ''
+                }
+            }
+        }
+    });
+
     createSectionText = (title, content) => {
         return (
-            <div>
+            <section>
                 <Row>
                     <Col>
-                        <Typography variant='h3' className='sectionTitle' gutterBottom style={{ color: '#fff', textAlign: 'center', fontFamily: `Lobster` }}>{title}</Typography>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Typography variant='body1' color='primary' gutterBottom style={{ textAlign: 'center', fontFamily: `ABeeZee` }}>
-                            {content}
+                        <Typography variant='h3' className='sectionTitle' gutterBottom style={{ color: '#fff', textAlign: 'center', fontFamily: `Lobster` }}>
+                            {this.props.language === 'pt-br' ? title.pt : title.en}
                         </Typography>
                     </Col>
                 </Row>
-            </div>
+                <Row>
+                    <Col>
+                        {this.props.language === 'pt-br' ? content.pt : content.en}
+                    </Col>
+                </Row>
+            </section>
         )
     }
 
     createSkillBar = (percent, skillName) => {
         let radioButtons = Math.ceil(percent / 10);
         return (
-            <div style={{ width: '100%' }}>
+            <div key={skillName} style={{ width: '100%' }}>
                 <MuiThemeProvider theme={this.typoCustomStyle()}>
                     <Typography variant='overline' color='primary'>{skillName}:</Typography>
                 </MuiThemeProvider>
@@ -75,6 +91,13 @@ class Resume extends Component {
             </div>
         );
     }
+
+    createProjectItem = ({ name, description, url }) => {
+        return (<ListItem key={description} button onClick={() => window.open(url, '__blank')}>
+            <ListItemText primary={name} secondary={description} />
+        </ListItem>)
+    }
+
 
     radioCustomStyle = () => createMuiTheme({
         typography: {
@@ -98,16 +121,57 @@ class Resume extends Component {
     render() {
 
         const skillList = [
-            { name: 'Github', value: 90 },
-            { name: 'Inglês', value: 90 },
+            { name: 'Github', value: 70 },
+            { name: this.props.language === 'pt-br' ? 'Inglês' : 'English', value: 90 },
             { name: 'React+Redux', value: 60 },
             { name: 'Javascript', value: 60 },
             { name: 'SQL & PL/SQL', value: 80 },
             { name: 'Express', value: 60 },
+            { name: 'React Native', value: 25 }
         ]
 
+        const projectList = [
+            {
+                name: 'OmniStackWeek',
+                description: this.props.language === 'pt-br' ?
+                    'Um projeto completo unindo React, React Native e Express. Que imita o Instagram com atualizações em tempo real.' :
+                    'An React, React Native and Express project. Inspired on Instagram with updates in real-time.',
+                url: 'https://github.com/Victor-R/OmniStackWeek'
+            },
+            {
+                name: 'App Suprimentos',
+                description: this.props.language === 'pt-br' ?
+                    'Uma aplicação React + Redux, de um portal de suprimentos.' :
+                    'An React + Redux Application of a provision company portal.',
+                url: 'https://github.com/Victor-R/app_suprimentos'
+            },
+            {
+                name: 'Projeto-DOOM',
+                description: this.props.language === 'pt-br' ?
+                    'Um simples projeto que recria o fogo do jogo DOOM, com uma table em HTML e código em Javascript.' :
+                    'A simple project that recreates the fire of the game DOOM, with a table in HTML and Javascript code.',
+                url: 'https://github.com/Victor-R/Projeto-DOOM'
+            },
+            {
+                name: 'trabalhoIA',
+                description: this.props.language === 'pt-br' ?
+                    'Sistema de simulação de resgate, simulação de interação multiagentes. O objetivo do projeto é criar estratégias em uma situação de desastre. (Java)' :
+                    'System of rescue simulation and multiagent interation simulation. The objective is to create strategies in a disaster event. (Java)',
+                url: 'https://github.com/Victor-R/trabalhoIA'
+            },
+            {
+                name: 'bioSense',
+                description: this.props.language === 'pt-br' ?
+                    'Projeto academico em Java onde é simulado sensores em uma Estufa' :
+                    'Academic Project in Java where sensors are simulated in a greenhouse',
+                url: 'https://github.com/Victor-R/bioSense'
+            }
+
+        ]
+
+
         return (
-            <Paper style={{ marginTop: '3em' }}>
+            <Paper className='main-Resume'>
                 <Row className='d-flex justify-content-center' style={{ marginBottom: '1em' }}>
                     <Col>
                         <Button variant='text' className='btn-block' onClick={this.props.handleBtnShowResume}><KeyboardArrowUp /></Button>
@@ -126,7 +190,17 @@ class Resume extends Component {
                             <Col lg='9'>
                                 <MuiThemeProvider theme={this.typoCustomStyle()}>
                                     <Typography variant='subtitle1' color='primary' style={{ textAlign: 'center' }}>Victor Henrique Ribeiro</Typography>
-                                    <Typography variant='subtitle2' color='primary' gutterBottom style={{ textAlign: 'center' }}>Analista de Sistemas</Typography>
+                                    <Typography variant='subtitle2' color='primary' gutterBottom style={{ textAlign: 'center' }}>
+                                        {this.props.language === 'pt-br' ? 'Analista de Sistemas' : 'System Analyst'}
+                                    </Typography>
+                                    <div className="d-flex justify-content-center">
+                                        <IconButton onClick={() => window.open('https://github.com/Victor-R/', '__blank')} color='primary'>
+                                            <FaGithub />
+                                        </IconButton>
+                                        <IconButton onClick={() => window.open('https://www.linkedin.com/in/victor-henrique-ribeiro-34172811a/', '__blank')} color='primary'>
+                                            <FaLinkedin />
+                                        </IconButton>
+                                    </div>
                                 </MuiThemeProvider>
                             </Col>
                         </Row>
@@ -147,13 +221,80 @@ class Resume extends Component {
                     <Col lg='8'>
                         <MuiThemeProvider theme={this.typoCustomStyle()}>
                             {
-                                this.createSectionText(`About me`, `Atuo à 2 anos como programador, onde realizei projetos na área de Web, Oracle ERP e banco de dados.`)
+                                this.createSectionText(
+                                    { pt: 'Sobre mim', en: `About me` },
+                                    {
+                                        pt: <Typography variant='body1' color='primary' gutterBottom style={{ textAlign: 'center', fontFamily: `ABeeZee` }}>
+                                            Olá meu nome é Victor, sou Analista de Sistemas e desenvolvedor de software. Desde que realizei meu Técnido em Informática, me apaixonei por programação.<br />
+                                            Atualmente, formando em Ciência da Computação pela UNESP, tenho feito alguns projetos para expandir meus horizontes como desenvolvedor, utilizando ferramentas como:
+                                            React, React Redux, React Native, Express, Java.<br /> Você pode acompanhar alguns dos meus projetos na seção "Projetos Recentes".
+                                            </Typography>,
+                                        en: <Typography variant='body1' color='primary' gutterBottom style={{ textAlign: 'center', fontFamily: `ABeeZee` }}>Hi my name is Victor, I'm System Analyst and software developer. Since I've finished my IT course, I fell in love with programming.<br />
+                                            Nowadays, concluding Computer Science degree on UNESP, I've been doing some projects to expand my horizons as a developer,
+                                        with technologies as: React, React Redux, React Native, Express, Java.<br /> You can follow some of my projects in section "Recent Projects".</Typography>
+                                    },
+
+                                )
                             }
                             {
-                                this.createSectionText(`Education`, `body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.`)
+                                this.createSectionText(
+                                    { pt: 'Formação', en: `Education` },
+                                    {
+                                        pt: <Typography variant='body1' color='primary' gutterBottom style={{ textAlign: 'center', fontFamily: `ABeeZee` }}>
+                                            Bacharelado em Ciência da Computação – Junho, 2019 Universidade Estadual Paulista – UNESP – Rio Claro, SP <br />
+                                            Curso Técnico em Tecnologia da Informação – Dezembro, 2012 ETEC – Escola Técnica Dep. Ary de Camargo Pedroso – Piracicaba, SP
+                                            </Typography>,
+                                        en: <Typography variant='body1' color='primary' gutterBottom style={{ textAlign: 'center', fontFamily: `ABeeZee` }}>
+                                            Bacharel in Computer Science - June 2019 Universidade Estadual Paulista – UNESP <br />
+                                            Technical Course in Information Technology IT – December 2012 ETEC – Escola Técnica Dep. Ary de Camargo Pedroso – Piracicaba, SP
+                                            </Typography>
+                                    },
+                                )
                             }
                             {
-                                this.createSectionText(`Work Experience`, `body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.`)
+                                this.createSectionText(
+                                    { pt: 'Experiência Profissional', en: `Work Experience` },
+                                    {
+                                        pt: <Typography variant='body1' color='primary' gutterBottom style={{ textAlign: 'center', fontFamily: `ABeeZee` }}>
+                                            <li>GAtec S/A – Gestão Agroindustrial; Agosto/2016 até Dezembro/2016; Infra - Estágio</li>
+                                            <li>CENTRUS – Central do SUS – Prefeitura de Piracicaba; Junho/2015 até Julho/2016; Infra - Estágio</li>
+                                            <li>Icaro Tech – Serviços e Comércio; Janeiro/2017 até Junho/2017; Analista de Suporte - Estágio</li>
+                                            <li><b>DEDINI – Indústrias de Base; Agosto/2017 – até o momento; Analista de Sistema Júnior</b></li>
+                                        </Typography>,
+                                        en: <Typography variant='body1' color='primary' gutterBottom style={{ textAlign: 'center', fontFamily: `ABeeZee` }}>
+                                            <li>GAtec S/A – Gestão Agroindustrial; August/2016 until December/2016; Infrastructure - Internship</li>
+                                            <li>CENTRUS – Central do SUS – Prefeitura de Piracicaba; June/2015 until July/2016; Infrastructure - Internship</li>
+                                            <li>Icaro Tech – Serviços e Comércio; January/2017 until June/2017; Support Analyst - Internship</li>
+                                            <li><b>DEDINI – Indústrias de Base; august/2017 – até o momento; Júnior System Analyst</b></li>
+                                        </Typography>
+                                    },
+
+                                )
+                            }
+                            {
+                                this.createSectionText(
+                                    { pt: 'Projetos Recentes', en: `Recent Projects` },
+                                    {
+                                        pt: <MuiThemeProvider theme={this.btnCustomStyles()}>
+                                            <Button className='btn-block' onClick={() => window.open('https://github.com/Victor-R/', '__blank')} color='primary' variant='contained'>
+                                                Acesse minha página do Github&nbsp;<FaGithub size={25} />
+                                            </Button>
+                                            <List className={'project-list'}>
+                                                {
+                                                    projectList.map(project => this.createProjectItem(project))
+                                                }
+                                            </List>
+                                        </MuiThemeProvider>,
+                                        en: <MuiThemeProvider theme={this.btnCustomStyles()}>
+                                            <Button style={{marginBottom: '1em'}} className='btn-block' onClick={() => window.open('https://github.com/Victor-R/', '__blank')} color='primary' variant='contained'>Visit My Github Page&nbsp;<FaGithub size={25} /></Button>
+                                            <List className={'project-list'}>
+                                                {
+                                                    projectList.map(project => this.createProjectItem(project))
+                                                }
+                                            </List>
+                                        </MuiThemeProvider>
+                                    },
+                                )
                             }
                         </MuiThemeProvider>
                     </Col>
